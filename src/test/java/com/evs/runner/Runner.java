256c722ae38abbb1c238a619c774f07c98c580f9
ajit <ajeetyadav8092@gmail.com>
2024-02-05 21:50:41 +0530

    New Update

--- src/test/java/com/evs/runner/Runner.java
+++ src/test/java/com/evs/runner/Runner.java
@@ -8,40 +8,26 @@ import com.evs.test.SalesTestScript;
 import com.evs.utils.WebUtils;
 
 public class Runner {
-	@BeforeClass
-	public void reports() {
+	
+
+	@Test
+	public void Test() {
 		WebUtils wt = new WebUtils();
 		wt.createExtentReport();
-	}
-
-	@Test(priority = 0)
-	public void Test() {
-
 		// Marketing scripts
 
-		MarketingTestScripts mrkts = new MarketingTestScripts();
-		mrkts.vt001MarketingCreateNewLeads();
-
-	}
-
-	@Test(priority = 1)
-	public void marketingVerificationtest() {
-		MarketingTestScripts mrkts = new MarketingTestScripts();
-		mrkts.vtoo2VerifyMarketingLeads();
-	}
-
-	@Test(priority = 2)
-	public void createAccountTest() {
+		MarketingTestScripts mrkts1 = new MarketingTestScripts();
+		mrkts1.vt001MarketingCreateNewLeads();
+		MarketingTestScripts mrkts2 = new MarketingTestScripts();
+		mrkts2.vtoo2VerifyMarketingLeads();
 		MarketingTestScripts mrkts = new MarketingTestScripts();
 		mrkts.vt004CreateNewMarketingAccount();
-	}
-
-	@Test(priority = 3)
-	public void salesText() {
 		SalesTestScript crnsll = new SalesTestScript();
 		crnsll.vt003CreateSalesLead();
-//	
-
 	}
+
+	
+
+
 
 }
