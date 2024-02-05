29cd82ee8b108dfb3b6357bca70819816463f11d
ajit <ajeetyadav8092@gmail.com>
2024-02-05 22:03:30 +0530

    All seprete

--- src/test/java/com/evs/runner/Runner.java
+++ src/test/java/com/evs/runner/Runner.java
@@ -8,26 +8,40 @@ import com.evs.test.SalesTestScript;
 import com.evs.utils.WebUtils;
 
 public class Runner {
-	
-
-	@Test
-	public void Test() {
+	@BeforeClass
+	public void reports() {
 		WebUtils wt = new WebUtils();
 		wt.createExtentReport();
-		// Marketing scripts
-
-		MarketingTestScripts mrkts1 = new MarketingTestScripts();
-		mrkts1.vt001MarketingCreateNewLeads();
-		MarketingTestScripts mrkts2 = new MarketingTestScripts();
-		mrkts2.vtoo2VerifyMarketingLeads();
-		MarketingTestScripts mrkts = new MarketingTestScripts();
-		mrkts.vt004CreateNewMarketingAccount();
-		SalesTestScript crnsll = new SalesTestScript();
-		crnsll.vt003CreateSalesLead();
 	}
 
-	
+	@Test(priority = 0)
+	public void Test() {
 
+		// Marketing scripts
 
+		MarketingTestScripts mrkts = new MarketingTestScripts();
+		mrkts.vt001MarketingCreateNewLeads();
+
+	}
+
+	@Test(priority = 1)
+	public void marketingVerificationtest() {
+		MarketingTestScripts mrkts = new MarketingTestScripts();
+		mrkts.vtoo2VerifyMarketingLeads();
+	}
+
+	@Test(priority = 2)
+	public void createAccountTest() {
+		MarketingTestScripts mrkts = new MarketingTestScripts();
+		mrkts.vt004CreateNewMarketingAccount();
+	}
+
+	@Test(priority = 3)
+	public void salesText() {
+		SalesTestScript crnsll = new SalesTestScript();
+		crnsll.vt003CreateSalesLead();
+//	
+
+	}
 
 }
